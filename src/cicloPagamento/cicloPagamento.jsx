7cff735faa73257a979367a4c93637b3377e24d2
Alan Vieceli <alansvieceli@gmail.com>
2019-07-01 19:46:03 -0300

    Modificando componente cilco de pagamento

--- src/cicloPagamento/cicloPagamento.jsx
+++ src/cicloPagamento/cicloPagamento.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h1>Cilco de Pagamento</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+
+class CicloPagamento extends React.Component {
+
+    render (){
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamento' small='Cadastro'/>
+                <Content>
+                    Ciclos de Pagamento
+                </Content>
+            </div>
+        )
+    }
+
+}
+
+export default CicloPagamento
