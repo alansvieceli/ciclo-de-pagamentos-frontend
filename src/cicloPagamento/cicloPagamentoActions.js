f0297bc925f55ccc443b539970940bf95e22ed3d
Alan Vieceli <alansvieceli@gmail.com>
2019-07-05 22:59:34 -0300

    Componente de Mensagens

--- src/cicloPagamento/cicloPagamentoActions.js
+++ src/cicloPagamento/cicloPagamentoActions.js
@@ -1,5 +1,6 @@
 import Constants from '../common/consts';
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 export function getList(){
     const request = axios.get(`${Constants.BASE_URL}`)
@@ -11,6 +12,14 @@ export function getList(){
 
 export function create(values){
     axios.post(`${Constants.BASE_URL}`, values)
+        .then(resp => {
+            toastr.success("Sucessos", "Operação Realizada com Sucessos.")
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(err => {
+                toastr.error("Erro", err)
+            });
+        })
     return {
         type: 'TEMP'
     }
