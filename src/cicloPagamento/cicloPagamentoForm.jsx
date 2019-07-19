70b1d17ef893c3d661bbf4e2b0d3b81bf04ec430
Alan Vieceli <alansvieceli@gmail.com>
2019-07-19 20:02:38 -0300

    Integrar soma de creditos e debitos

--- src/cicloPagamento/cicloPagamentoForm.jsx
+++ src/cicloPagamento/cicloPagamentoForm.jsx
@@ -11,9 +11,21 @@ import Sumary from './sumary'
 
 class CicloPagamentoForm extends React.Component {
 
+    calcularSumario(){
+        //técnica map-reduce
+        //+valor converte para valor
+        const sum = (t, v) => t + v
+        return {
+            somaDeCreditos: this.props.creditos.map(c => +c.valor || 0).reduce(sum),
+            somaDeDebitos: this.props.debitos.map(d => +d.valor || 0).reduce(sum)
+        }
+    }
+
     render(){
 
         const { handleSubmit, readOnly, creditos, debitos } = this.props //o reduxform add isso no props
+        const { somaDeCreditos, somaDeDebitos } = this.calcularSumario();
+
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -23,7 +35,7 @@ class CicloPagamentoForm extends React.Component {
                         label='Mês' cols='12 4' placeholder="Informe o mês"/>
                     <Field name='ano' component={labelAndInput} readOnly={readOnly} type='number'
                         label='Ano' cols='12 4' placeholder="Informe o ano"/>
-                    <Sumary credito={6500} debito={456} />
+                    <Sumary credito={somaDeCreditos} debito={somaDeDebitos} />
                     <ItemList cols="12 6" list={creditos} readOnly={readOnly}
                         field='creditos' caption='Créditos'/>
                     <ItemList cols="12 6" list={debitos} readOnly={readOnly}
